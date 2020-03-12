d506768276943eeb98c73b8aefa43c65346bf4ae
mactkg <mactkg@gmail.com>
2020-03-13 00:52:38 +0900

    feat: page icons, meta informations (#4)
    * add: Arch Research icons
    
    * Add page icon, ogp, meta data for twitter

---
+++ app/components/Meta.tsx
@@ -0,0 +1,54 @@
+import { FC } from "react";
+
+const root = "https://arch-inc.github.io/anitech2020-spring-sample";
+
+const Meta: FC = () => (
+  <>
+    <link
+      rel="icon"
+      type="image/svg+xml"
+      sizes="any"
+      href="/static/images/arch-research-sq-inverted.svg"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="any"
+      href="/static/images/arch-research-sq-inverted.png"
+    />
+    <link
+      rel="mask-icon"
+      type="image/svg+xml"
+      href="/static/images/arch-research-sq.svg"
+      color="#008c8c"
+    />
+    <link
+      rel="apple-touch-icon"
+      type="image/png"
+      sizes="any"
+      href="/static/images/arch-research-sq-inverted.png"
+    />
+
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:site" content="@ArchResearchJp" />
+    <meta
+      property="twitter:image"
+      content={`${root}/static/images/arch-research-sq-inverted.png`}
+    />
+
+    <meta
+      property="og:title"
+      content="anitech2020-spring-sample | Layer support example for Fabric.js"
+    />
+    <meta
+      property="og:image"
+      content={`${root}/static/images/arch-research-sq-inverted.png`}
+    />
+    <meta
+      property="og:description"
+      content="技術書典8で頒布予定の「アニメ技術 2020春」で紹介した、fabric.jsを使ったスケッチソフトのサンプルページです。"
+    />
+  </>
+);
+
+export default Meta;
